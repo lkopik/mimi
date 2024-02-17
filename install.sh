5187a2fceb5666a6803649e030383936a63f090f
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-17 23:10:04 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 sudo chmod +x Start-IronFish.sh
 sudo chmod +x Start-Nexa.sh
 sudo chmod +x lolMiner
+sudo chmod +x Start-Alephium.sh
+sudo chmod +x Start-Ergo.sh
+sudo chmod +x Start-Karlsen.sh
+sudo chmod +x Start-Kaspa.sh
+sudo chmod +x Start-PYRIN.sh
 sudo mv mi.service /etc/systemd/system/
 sudo mv im.service /etc/systemd/system/
 sudo systemctl daemon-reload
