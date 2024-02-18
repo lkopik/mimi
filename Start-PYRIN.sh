819b28934cd6986dc424c8da48d17290bc9c9ee2
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-18 23:46:44 +0500

    Update Start-PYRIN.sh

--- Start-PYRIN.sh
+++ Start-PYRIN.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./lolMiner --algo PYRIN --pool pyi.kryptex.network:7777 --user lkop228228@gmail.com
+./lolMiner --algo PYRIN --pool pyi.kryptex.network:7777 --user lkop228228@gmail.com --tstop 75
