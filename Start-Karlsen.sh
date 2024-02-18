419fd379a38b4d873a55bd8819eebe14fb2ec37b
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-18 23:48:59 +0500

    Update Start-Karlsen.sh

--- Start-Karlsen.sh
+++ Start-Karlsen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./lolMiner --algo KARLSEN --pool kls.kryptex.network:7777 --user lkop228228@gmail.com
+./lolMiner --algo KARLSEN --pool kls.kryptex.network:7777 --user lkop228228@gmail.com --tstop 75
