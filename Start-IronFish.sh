de75113e45195f4e0ea5d0f4d3e779dade5f4d1b
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-18 23:48:37 +0500

    Update Start-IronFish.sh

--- Start-IronFish.sh
+++ Start-IronFish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./lolMiner --algo IRONFISH --pool iron.kryptex.network:7777 --user lkop228228@gmail.com
+./lolMiner --algo IRONFISH --pool iron.kryptex.network:7777 --user lkop228228@gmail.com --tstop 75
