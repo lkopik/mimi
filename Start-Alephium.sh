2017ac3c5f846b1847b1fdeb730c79c01c19521f
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-18 23:47:55 +0500

    Update Start-Alephium.sh

--- Start-Alephium.sh
+++ Start-Alephium.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./lolMiner --algo ALEPH --pool alph.kryptex.network:7777 --user lkop228228@gmail.com/1
+./lolMiner --algo ALEPH --pool alph.kryptex.network:7777 --user lkop228228@gmail.com/1 --tstop 75
