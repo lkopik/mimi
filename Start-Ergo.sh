172664ed6bfc6e31a7714c249bd4845f76d7da1a
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-18 23:48:19 +0500

    Update Start-Ergo.sh

--- Start-Ergo.sh
+++ Start-Ergo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./lolMiner --algo AUTOLYKOS2 --pool erg.kryptex.network:7777 --user lkop228228@gmail.com/2
+./lolMiner --algo AUTOLYKOS2 --pool erg.kryptex.network:7777 --user lkop228228@gmail.com/2 --tstop 75
