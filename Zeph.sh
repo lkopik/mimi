b4cfa1b7447ebdfd6c72d97d2ef95ee27f7b24d4
lkopik <81908442+lkopik@users.noreply.github.com>
2024-03-10 00:45:07 +0500

    Update Zeph.sh

--- Zeph.sh
+++ Zeph.sh
@@ -1,1 +1,1 @@
-/xmrig --coin zephyr --url zeph.kryptex.network:8888 --user lkop228228@gmail.com/asd --tls -k
+./xmrig --coin zephyr --url zeph.kryptex.network:8888 --user lkop228228@gmail.com/asd --tls -k
